2f6796a2053d3828b35c4febf8d01121ce131d92
OodavidsinoO <david.sin@tuta.io>
2026-01-01 17:31:27 +0800

    Aerodactyl: overlay: Add Power Off Alarm resource and configuration

--- device-common.mk
+++ device-common.mk
@@ -262,6 +262,7 @@ PRODUCT_PACKAGES += \
     NothingFrameworksResTarget \
     NothingLauncher3ResTarget \
     NothingNfcResTarget \
+    NothingPowerOffAlarmResTarget \
     NothingSettingsResTarget \
     NothingSystemUIResTarget
 
@@ -297,6 +298,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+# Power Off Alarm
+PRODUCT_PACKAGES += \
+    PowerOffAlarm
 
 # Properties
 include $(LOCAL_PATH)/vendor_logtag.mk
