e985c31df8c1b76ca236efc86cf820bab47bf97b
Mashopy <eliasgheeraert@gmail.com>
2026-01-04 13:27:12 +0100

    Aerodactyl: Properly patch blobs related to Stagefright
    Audio primary HAL doesn't have anything related to Stagefright, so there is no need to add
    libstagefright_foundation-v33.so dependency int it. Same goes for the C2 service as
    it's the C2 vdec/venc and plugin libs that has references for it.
    
    And let's patch the sensor HAL too just in case.
    
    There is no need to patch the system libs that depends over it as they aren't built
    over Android 13 like the vendor libs are.

--- extract-files.py
+++ extract-files.py
@@ -52,8 +52,6 @@ blob_fixups: blob_fixups_user_type = {
         .apktool_patch('blob-patches/ImsService'),
     'vendor/bin/hw/android.hardware.graphics.composer@3.1-service': blob_fixup()
         .replace_needed('android.hardware.graphics.composer@2.1-resources.so', 'android.hardware.graphics.composer@2.1-resources-v34.so'),
-    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
-        .add_needed('libstagefright_foundation-v33.so'),
     'vendor/bin/hw/android.hardware.security.keymint@2.0-service.trustonic': blob_fixup()
         .add_needed('android.hardware.security.rkp-V2-ndk.so'),
     'vendor/bin/hw/mt6886/camerahalserver': blob_fixup()
@@ -68,13 +66,24 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('# STNFC_FW_CONF_NAME', 'STNFC_FW_CONF_NAME')
         .regex_replace('STNFC_FW_DEBUG_ENABLED=1', 'STNFC_FW_DEBUG_ENABLED=0'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
-        .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libalsautils.so', 'libalsautils-v33.so')
         .replace_needed('libtinyxml2.so', 'libtinyxml2-bp2a.so')
         .binary_regex_replace(b'A2dpsuspendonly', b'A2dpSuspended\x00\x00')
         .binary_regex_replace(b'BTAudiosuspend', b'A2dpSuspended\x00'),
     'vendor/lib64/hw/hwcomposer.mtk_common.so': blob_fixup()
         .add_needed('libprocessgroup_shim.so'),
+    (
+        'vendor/lib64/hw/sensors.mediatek.V2.0.so',
+        'vendor/lib64/libcodec2_mtk_c2store.so',
+        'vendor/lib64/libcodec2_mtk_vdec.so',
+        'vendor/lib64/libcodec2_mtk_venc.so',
+        'vendor/lib64/libcodec2_vpp_AIMEMC_plugin.so',
+        'vendor/lib64/libcodec2_vpp_AISR_plugin.so',
+        'vendor/lib64/libcodec2_vpp_mi_plugin.so',
+        'vendor/lib64/libcodec2_vpp_qt_plugin.so',
+        'vendor/lib64/libcodec2_vpp_rs_plugin.so'
+    ): blob_fixup()
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
     (
         'vendor/lib64/hw/mt6886/vendor.mediatek.hardware.pq_aidl-impl.so',
         'vendor/lib64/mt6886/libmmlpqImpl.so',
